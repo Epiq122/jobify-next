f369c16ab9828c06df2940a3dc7edac5abf3c660
Robert Gleason <l2ob122@gmail.com>
2024-02-25 17:56:55 -0800

    Add dropdown menu for links in Navbar, created links dropdown

--- components/LinksDropdown.tsx
+++ components/LinksDropdown.tsx
@@ -1,4 +1,40 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import { AlignLeft } from 'lucide-react';
+import { Button } from './ui/button';
+import links from '@/utils/links';
+import Link from 'next/link';
+
 const LinksDropdown = () => {
-  return <div>LinksDropdown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className='lg:hidden'>
+        <Button>
+          <AlignLeft />
+          <span className='sr-only'>Toggle links</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className='w-52 lg:hidden'
+        align='start'
+        sideOffset={25}
+      >
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem key={link.href}>
+              <Link href={link.href} className='flex items-center gap-x-2 '>
+                {link.icon}
+                <span className='capitalize'>{link.name}</span>
+              </Link>
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 export default LinksDropdown;
